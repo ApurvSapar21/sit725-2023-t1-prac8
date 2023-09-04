b42bc3ec37ddcd29da13d757dc7c0a574160a686
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2023-09-05 00:25:56 +1000

    Addition of Socket

--- server.js
+++ server.js
@@ -5,13 +5,28 @@ let port = process.env.port || 3000;
 require('./db.connection');
 let router = require('./routers/router');
 
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/cat', router);
 
-app.listen(port, () => {
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(()=>{
+        socket.emit('number', parseInt(Math.random()*10));
+    }, 1000)
+});
+
+
+http.listen(port, () => {
     console.log('express server started');
 });
 
